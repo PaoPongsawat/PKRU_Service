68539f860e61927ea62ad2db45d8a57fda9a9f9f
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-28 11:34:32 +0700

    Create and Connected Signup OK

---
+++ PKRU_Service/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  PKRU_Service
+//
+//  Created by Com422Mac10 on 7/28/2560 BE.
+//  Copyright © 2560 Com422Mac10. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
